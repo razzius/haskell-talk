a7204971ff8666f61f350e02c751008286314fd7
Razzi Abuissa <razzi53@gmail.com>
2017-04-04 23:34:45 -0700

    Script for getting jira issue

---
+++ jira_issue.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Prelude hiding (putStrLn)
+
+import Data.ByteString.Char8 (pack)
+import Data.Maybe (Maybe(Just, Nothing))
+import Data.Text.Internal (Text)
+import Data.Text.IO (putStrLn)
+
+import System.Environment (lookupEnv)
+import System.Exit (exitFailure)
+
+import Network.Wreq (getWith, defaults, header, param, responseBody)
+import Control.Lens ((^.), (.~), (&))
+import Data.Aeson.Lens (key, _String, nth)
+
+
+jql = "assignee = razzi\
+      \ AND status in (\"In Development\", \"Ready For Development\")\
+      \ AND issuetype != epic and sprint in opensprints()\
+      \ ORDER BY status, sprint"
+
+
+opts auth = defaults & header "Authorization" .~ [pack auth]
+                     & param "maxResults" .~ ["1"]
+                     & param "jql" .~ [jql]
+
+
+getIssue :: String -> IO Text
+getIssue auth = do
+  response <- getWith (opts auth) "https://sighten.atlassian.net/rest/api/2/search"
+  return $ response ^. responseBody . key "issues" . nth 0 . key "fields" . key "description" . _String
+
+
+main = do
+  jiraAuth <- lookupEnv "JIRA_AUTH"
+
+  case jiraAuth of
+    Nothing ->
+      putStrLn "Need to set JIRA_AUTH" >>
+      exitFailure
+    Just auth -> getIssue auth >>= putStrLn
